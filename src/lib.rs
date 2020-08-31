658df869721831fb4ee534049dcdf749fef5c622
szunami <sam.szuflita@gmail.com>
2020-08-31 13:24:37 +0000

    Add basic parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-#[derive(PartialEq, Debug``)]
+#[derive(PartialEq, Debug)]
 struct Crossword {
     contents: String,
     width: usize,
@@ -41,12 +41,84 @@ impl fmt::Display for Crossword {
 }
 
 fn fill_crossword(crossword: &Crossword) -> Crossword {
+    // parse crossword into partially filled words
+    // fill a word
     crossword.clone()
+}
+
+fn parse_words(crossword: &Crossword) -> Vec<Word> {
+    let mut result = vec![];
+
+    let byte_array = crossword.contents.as_bytes();
+
+    let mut current_word = "".to_owned();
+    let mut start_row = None;
+    let mut start_col = None;
+    let mut length = 0;
+
+    for row in 0..crossword.height {
+        for col in 0..crossword.width {
+            let current_char = byte_array[row * crossword.width + col] as char;
+            if current_char != '*' {
+                // found a char; is it our first?
+                if start_row == None {
+                    start_row = Some(row);
+                    start_col = Some(col);
+                }
+                length += 1;
+                current_word.push(current_char)
+            } else {
+                let new_word = Word {
+                    contents: current_word,
+                    start_row: start_row.unwrap(),
+                    start_col: start_col.unwrap(),
+                    length: length,
+                    direction: Direction::Across,
+                };
+                result.push(new_word);
+                current_word = "".to_owned();
+                length = 0;
+                start_row = None;
+                start_col = None;
+            }
+        }
+        // have to process end of row
+        if current_word.len() > 0 {
+            let new_word = Word {
+                contents: current_word,
+                start_row: start_row.unwrap(),
+                start_col: start_col.unwrap(),
+                length: length,
+                direction: Direction::Across,
+            };
+            result.push(new_word);
+            current_word = "".to_owned();
+            length = 0;
+            start_row = None;
+            start_col = None;
+        }
+    }
+
+    result
+}
+
+#[derive(Debug, PartialEq)]
+enum Direction {
+    Across,
+    Down,
+}
+#[derive(Debug, PartialEq)]
+struct Word {
+    contents: String,
+    start_row: usize,
+    start_col: usize,
+    length: usize,
+    direction: Direction,
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{fill_crossword, Crossword};
+    use crate::{fill_crossword, parse_words, Crossword, Word};
 
     #[test]
     fn it_works() {
@@ -67,5 +139,37 @@ mod tests {
             height: 3,
         };
         assert_eq!(fill_crossword(&c), c);
+    }
+
+    #[test]
+    fn parse_works() {
+        let c = Crossword {
+            contents: String::from("abcdefghi"),
+            width: 3,
+            height: 3,
+        };
+        let result = parse_words(&c);
+
+        assert_eq!(result.len(), 3);
+        assert_eq!(
+            result[0],
+            Word {
+                contents: String::from("abc"),
+                start_col: 0,
+                start_row: 0,
+                length: 3,
+                direction: crate::Direction::Across
+            }
+        );
+        assert_eq!(
+            result[1],
+            Word {
+                contents: String::from("def"),
+                start_col: 0,
+                start_row: 1,
+                length: 3,
+                direction: crate::Direction::Across
+            }
+        );
     }
 }
