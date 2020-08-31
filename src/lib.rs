41e8ebc06dfb9aa7ff0243848eb37c0edc13cbe0
szunami <sam.szuflita@gmail.com>
2020-08-31 02:07:41 +0000

    Added fill

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,31 @@
 use std::fmt;
 
+#[derive(PartialEq, Debug``)]
 struct Crossword {
     contents: String,
     width: usize,
     height: usize,
 }
 
+impl Clone for Crossword {
+    fn clone(&self) -> Self {
+        Crossword {
+            contents: self.contents.clone(),
+            width: self.width,
+            height: self.height,
+        }
+    }
+}
+
 impl fmt::Display for Crossword {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         for row in 0..self.height {
             for col in 0..self.width {
-                write!(f, "{}", self.contents.as_bytes()[row * self.width + col] as char);
+                write!(
+                    f,
+                    "{}",
+                    self.contents.as_bytes()[row * self.width + col] as char
+                );
                 if col != self.width - 1 {
                     write!(f, " ");
                 }
@@ -25,9 +40,13 @@ impl fmt::Display for Crossword {
     }
 }
 
+fn fill_crossword(crossword: &Crossword) -> Crossword {
+    crossword.clone()
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::Crossword;
+    use crate::{fill_crossword, Crossword};
 
     #[test]
     fn it_works() {
@@ -38,5 +57,15 @@ mod tests {
         };
 
         println!("{}", c);
+    }
+
+    #[test]
+    fn fill_works() {
+        let c = Crossword {
+            contents: String::from("         "),
+            width: 3,
+            height: 3,
+        };
+        assert_eq!(fill_crossword(&c), c);
     }
 }
