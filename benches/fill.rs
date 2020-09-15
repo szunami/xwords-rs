43678f69f8743901ce233a38f4a4743c58155882
szunami <sam.szuflita@gmail.com>
2020-09-15 13:07:03 +0000

    Target find_fills in benchmark

--- benches/fill.rs
+++ benches/fill.rs
@@ -2,9 +2,9 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use xwords::{find_fills, Word, Direction};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| {
+    c.bench_function("find_fills", |b| {
         b.iter(|| {
-            find_fills(Word::new(String::from("R**"), 0, 0, 3, Direction::Across))
+            find_fills(Word::new(String::from("R  "), 0, 0, 3, Direction::Across))
         })
     });
 }
