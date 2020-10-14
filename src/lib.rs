0bc6e1fc0d8baec43e2e74837a84b84d6a01a51c
Sam Szuflita <sam.szuflita@alloy.us>
2020-10-14 13:53:46 -0400

    Parse boundaries

--- src/lib.rs
+++ src/lib.rs
@@ -364,6 +364,109 @@ fn parse_words(crossword: &Crossword) -> Vec<Word> {
     result
 }
 
+fn parse_word_boundaries(crossword: &Crossword) -> Vec<WordBoundary> {
+    let mut result = vec![];
+
+    let byte_array = crossword.contents.as_bytes();
+
+    let mut start_row = None;
+    let mut start_col = None;
+    let mut length = 0;
+
+    for row in 0..crossword.height {
+        for col in 0..crossword.width {
+            let current_char = byte_array[row * crossword.width + col] as char;
+            if current_char != '*' {
+                // found a char; is it our first?
+                if start_row == None {
+                    start_row = Some(row);
+                    start_col = Some(col);
+                }
+                length += 1;
+            } else {
+                // If we don't have any data yet, just keep going
+                if start_row == None {
+                    continue;
+                }
+                let new_word = WordBoundary {
+                    start_row: start_row.unwrap(),
+                    start_col: start_col.unwrap(),
+                    length: length,
+                    direction: Direction::Across,
+                };
+                result.push(new_word);
+                length = 0;
+                start_row = None;
+                start_col = None;
+            }
+        }
+        // have to process end of row
+        if length > 0 {
+            let new_word = WordBoundary {
+                start_row: start_row.unwrap(),
+                start_col: start_col.unwrap(),
+                length: length,
+                direction: Direction::Across,
+            };
+            result.push(new_word);
+            length = 0;
+            start_row = None;
+            start_col = None;
+        }
+    }
+
+    for col in 0..crossword.width {
+        for row in 0..crossword.height {
+            let current_char = byte_array[row * crossword.width + col] as char;
+            if current_char != '*' {
+                // found a char; is it our first?
+                if start_row == None {
+                    start_row = Some(row);
+                    start_col = Some(col);
+                }
+                length += 1;
+            } else {
+                if start_row == None {
+                    continue;
+                }
+                let new_word = WordBoundary {
+                    start_row: start_row.unwrap(),
+                    start_col: start_col.unwrap(),
+                    length: length,
+                    direction: Direction::Down,
+                };
+                result.push(new_word);
+                length = 0;
+                start_row = None;
+                start_col = None;
+            }
+        }
+        // have to process end of row
+        if length > 0 {
+            let new_word = WordBoundary {
+                start_row: start_row.unwrap(),
+                start_col: start_col.unwrap(),
+                length: length,
+                direction: Direction::Down,
+            };
+            result.push(new_word);
+            length = 0;
+            start_row = None;
+            start_col = None;
+        }
+    }
+
+    return result;
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct WordBoundary {
+    start_row: usize,
+    start_col: usize,
+    length: usize,
+    direction: Direction,
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Direction {
     Across,
@@ -423,7 +526,7 @@ mod tests {
 
         use std::time::Instant;
 
-use crate::ALL_WORDS;
+use crate::{ALL_WORDS, WordBoundary, parse_word_boundaries};
 use crate::{Crossword, Direction, Word, fill_crossword, fill_one_word, find_fills, is_viable, parse_words};
 
     #[test]
@@ -535,6 +638,54 @@ use crate::{Crossword, Direction, Word, fill_crossword, fill_one_word, find_fill
             result[3],
             Word {
                 contents: String::from("adg"),
+                start_col: 0,
+                start_row: 0,
+                length: 3,
+                direction: Direction::Down,
+            }
+        )
+    }
+
+    #[test]
+    fn parse_word_boundaries_works() {
+        let c = Crossword {
+            contents: String::from("abcdefghi"),
+            width: 3,
+            height: 3,
+        };
+        let result = parse_word_boundaries(&c);
+
+        assert_eq!(result.len(), 6);
+        assert_eq!(
+            result[0],
+            WordBoundary {
+                start_col: 0,
+                start_row: 0,
+                length: 3,
+                direction: Direction::Across
+            }
+        );
+        assert_eq!(
+            result[1],
+            WordBoundary {
+                start_col: 0,
+                start_row: 1,
+                length: 3,
+                direction: Direction::Across
+            }
+        );
+        assert_eq!(
+            result[2],
+            WordBoundary {
+                start_col: 0,
+                start_row: 2,
+                length: 3,
+                direction: Direction::Across
+            }
+        );
+        assert_eq!(
+            result[3],
+            WordBoundary {
                 start_col: 0,
                 start_row: 0,
                 length: 3,
