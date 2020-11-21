aeabd8e31b43d9557d5b6832291ea30f0903dcb5
Sam Szuflita <sam.szuflita@gmail.com>
2020-11-20 19:12:07 -0500

    Fix empty & sample

--- examples/empty.rs
+++ examples/empty.rs
@@ -1,52 +1,55 @@
-// use std::fs::File;
-// use std::sync::Arc;
-// use std::time::Instant;
-// use xwords::crossword::Crossword;
-// use xwords::default_indexes;
-// use xwords::fill::parallel::fill_crossword;
+use xwords::fill::Filler;
+use xwords::fill::parallel::ParallelFiller;
+use std::fs::File;
+use std::sync::Arc;
+use std::time::Instant;
+use xwords::crossword::Crossword;
+use xwords::default_indexes;
 
-// fn main() {
-//     let now = Instant::now();
+fn main() {
+    let now = Instant::now();
 
-//     let guard = pprof::ProfilerGuard::new(1000).unwrap();
-//     std::thread::spawn(move || loop {
-//         match guard.report().build() {
-//             Ok(report) => {
-//                 let file = File::create("flamegraph.svg").unwrap();
-//                 report.flamegraph(file).unwrap();
-//             }
-//             Err(_) => {}
-//         };
-//         std::thread::sleep(std::time::Duration::from_secs(5))
-//     });
+    let guard = pprof::ProfilerGuard::new(1000).unwrap();
+    std::thread::spawn(move || loop {
+        match guard.report().build() {
+            Ok(report) => {
+                let file = File::create("flamegraph.svg").unwrap();
+                report.flamegraph(file).unwrap();
+            }
+            Err(_) => {}
+        };
+        std::thread::sleep(std::time::Duration::from_secs(5))
+    });
 
-//     let real_puz = Crossword::new(String::from(
-//         "
-//     *    *
-//     *    *
-//          *
-//    *   *   *
-// **    *
-//       *     ***
-//      *    *
-//    *       *
-//     *    *
-// ***     *
-//         *    **
-//    *   *   *
-//      *
-//      *    *
-//      *    *
-// ",
-//     ))
-//     .unwrap();
+    let real_puz = Crossword::new(String::from(
+        "
+    *    *
+    *    *
+         *
+   *   *   *
+**    *
+      *     ***
+     *    *
+   *       *
+    *    *
+***     *
+        *    **
+   *   *   *
+     *
+     *    *
+     *    *
+",
+    ))
+    .unwrap();
 
-//     println!("{}", real_puz);
+    println!("{}", real_puz);
 
-//     let (bigrams, trie) = default_indexes();
-//     println!("Loaded indices in {}ms", now.elapsed().as_millis());
+    let (bigrams, trie) = default_indexes();
+    println!("Loaded indices in {}ms", now.elapsed().as_millis());
+    
+    let filler = ParallelFiller::new(Arc::new(trie), Arc::new(bigrams));
 
-//     let filled_puz = fill_crossword(&real_puz, Arc::new(trie), Arc::new(bigrams)).unwrap();
-//     println!("Filled in {} seconds.", now.elapsed().as_secs());
-//     println!("{}", filled_puz);
-// }
+    let filled_puz = filler.fill(&real_puz).unwrap();
+    println!("Filled in {} seconds.", now.elapsed().as_secs());
+    println!("{}", filled_puz);
+}
--- examples/sample.rs
+++ examples/sample.rs
@@ -1,39 +1,41 @@
-// use std::sync::Arc;
-// use std::time::Instant;
-// use xwords::crossword::Crossword;
-// use xwords::default_indexes;
-// use xwords::fill::parallel::fill_crossword;
+use xwords::fill::Filler;
+use xwords::fill::parallel::ParallelFiller;
+use std::sync::Arc;
+use std::time::Instant;
+use xwords::crossword::Crossword;
+use xwords::default_indexes;
 
-// fn main() {
-//     let now = Instant::now();
+fn main() {
+    let now = Instant::now();
 
-//     let real_puz = Crossword::new(String::from(
-//         "
-//   S *F  N*B
-//   E *L  O*A
-//          *N
-//   V* W *E D*
-// **E  E*
-// RATEDR*     ***
-//   I  *B N * C
-//   M*       *R
-//   E * L S*
-// ***ACIDY*GRATES
-//         *A  I**
-// KIA*  A* R *C
-// EVILS*
-// B    *L  E* S
-// YAYAS*E  N* M
-// ",
-//     ))
-//     .unwrap();
+    let real_puz = Crossword::new(String::from(
+        "
+  S *F  N*B
+  E *L  O*A
+         *N
+  V* W *E D*
+**E  E*
+RATEDR*     ***
+  I  *B N * C
+  M*       *R
+  E * L S*
+***ACIDY*GRATES
+        *A  I**
+KIA*  A* R *C
+EVILS*
+B    *L  E* S
+YAYAS*E  N* M
+",
+    ))
+    .unwrap();
 
-//     println!("{}", real_puz);
+    println!("{}", real_puz);
 
-//     let (bigrams, trie) = default_indexes();
-//     println!("Loaded indices in {}ms", now.elapsed().as_millis());
+    let (bigrams, trie) = default_indexes();
+    println!("Loaded indices in {}ms", now.elapsed().as_millis());
 
-//     let filled_puz = fill_crossword(&real_puz, Arc::new(trie), Arc::new(bigrams)).unwrap();
-//     println!("Filled in {} seconds.", now.elapsed().as_secs());
-//     println!("{}", filled_puz);
-// }
+    let filler = ParallelFiller::new(Arc::new(trie), Arc::new(bigrams));
+    let filled_puz = filler.fill(&real_puz).unwrap();
+    println!("Filled in {} seconds.", now.elapsed().as_secs());
+    println!("{}", filled_puz);
+}
