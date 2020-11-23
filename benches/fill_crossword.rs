452f7a29fbedb2e494f68703f13f9ae896a78f7c
Sam Szuflita <sam.szuflita@gmail.com>
2020-11-23 09:04:15 -0500

    Remove bad bench

--- benches/fill_crossword.rs
+++ benches/fill_crossword.rs
@@ -65,36 +65,6 @@ YAYAS*ETON* M
             b.iter(|| filler.fill(black_box(&input)));
         }),
     );
-
-    let tmp_bigrams = bigrams.clone();
-    let tmp_trie = trie.clone();
-    c.bench(
-        "fill_crosswords",
-        Benchmark::new("empty_20201012_crossword", move |b| {
-            let input = Crossword::new(String::from(
-                "
-    *    *     
-    *    *     
-         *     
-   *   *   *   
-**    *        
-      *     ***
-     *    *    
-   *       *   
-    *    *     
-***     *      
-        *    **
-   *   *   *   
-     *         
-     *    *    
-     *    *    
-",
-            ))
-            .unwrap();
-            let filler = ParallelFiller::new(tmp_trie.clone(), tmp_bigrams.clone());
-            b.iter(|| filler.fill(black_box(&input)));
-        }),
-    );
 }
 
 criterion_group!(benches, criterion_benchmark);
