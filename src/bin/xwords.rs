1c13133367ef812ee3f5c2a02c1d6595ce6159a0
Sam Szuflita <sam.szuflita@gmail.com>
2020-11-24 16:06:42 -0500

    fmt

--- src/bin/xwords.rs
+++ src/bin/xwords.rs
@@ -1,8 +1,15 @@
 extern crate clap;
-use std::{sync::Arc, fs::File};
+use std::{fs::File, sync::Arc};
 
 use clap::{App, Arg};
-use xwords::{crossword::Crossword, default_indexes, fill::Filler, fill::{parallel::ParallelFiller, simple::SimpleFiller, single_threaded::SingleThreadedFiller}};
+use xwords::{
+    crossword::Crossword,
+    default_indexes,
+    fill::{
+        parallel::ParallelFiller, simple::SimpleFiller, single_threaded::SingleThreadedFiller,
+        Filler,
+    },
+};
 
 fn main() -> Result<(), String> {
     let matches = App::new("xwords")
@@ -21,21 +28,23 @@ fn main() -> Result<(), String> {
                 .value_name("ALGORITHM")
                 .possible_values(&["simple", "single_threaded", "parallel"])
                 .default_value("single_threaded"),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("profile")
-            .short("p")
-            .long("profile")
-            .takes_value(false)
+                .short("p")
+                .long("profile")
+                .takes_value(false),
         )
         .get_matches();
-        
+
     let input = matches.value_of("input").expect("input not included");
     let input = std::fs::read_to_string(input).expect("failed to read input");
     let input = Crossword::new(input).expect("failed to parse input");
 
-    let algorithm = matches.value_of("algorithm").expect("failed to load algorithm");
-    
-    
+    let algorithm = matches
+        .value_of("algorithm")
+        .expect("failed to load algorithm");
+
     if matches.is_present("profile") {
         let guard = pprof::ProfilerGuard::new(1000).unwrap();
         std::thread::spawn(move || loop {
@@ -49,30 +58,30 @@ fn main() -> Result<(), String> {
             std::thread::sleep(std::time::Duration::from_secs(5))
         });
     }
-    
+
     let output = match algorithm {
         "single_threaded" => {
             let (bigrams, trie) = default_indexes();
             SingleThreadedFiller::new(&trie, &bigrams).fill(&input)
-        },
+        }
         "simple" => {
             let (_bigrams, trie) = default_indexes();
             SimpleFiller::new(&trie).fill(&input)
-        },
+        }
         "parallel" => {
             let (bigrams, trie) = default_indexes();
             ParallelFiller::new(Arc::new(trie), Arc::new(bigrams)).fill(&input)
-        },
+        }
         _ => {
             return Err(String::from("Failed to parse algorithm"));
         }
     };
-    
+
     match output {
         Ok(output) => {
             println!("{}", output);
-        },
-        Err(_) => {return Err(String::from("Failed to fill crossword"))}
+        }
+        Err(_) => return Err(String::from("Failed to fill crossword")),
     }
     Ok(())
 }
