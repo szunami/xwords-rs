38272012c9bcad59f1ff2fdad146b7af9d5f05b5
Sam Szuflita <sam.szuflita@gmail.com>
2020-11-27 18:38:38 -0500

    Add more benches

--- benches/fill_crossword_simple.rs
+++ benches/fill_crossword_simple.rs
@@ -15,59 +15,14 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
     let tmp_trie = trie.clone();
 
-    let input = Crossword::new(String::from("         ")).unwrap();
     c.bench(
         "simple_filler",
-        Benchmark::new("fill_3x3_crossword", move |b| {
+        Benchmark::new("fill_20201005_crossword", move |b| {
             let mut filler = SimpleFiller::new(tmp_trie.as_ref());
 
-            b.iter(|| {
-                assert!(filler.fill(black_box(&input)).is_ok());
-            });
-        }),
-    );
-
-    let input = Crossword::new(String::from("                ")).unwrap();
-    let tmp_trie = trie.clone();
-
-    c.bench(
-        "simple_filler",
-        Benchmark::new("fill_4x4_crossword", move |b| {
-            let mut filler = SimpleFiller::new(tmp_trie.as_ref());
-
-            b.iter(|| {
-                assert!(filler.fill(black_box(&input)).is_ok());
-            });
-        }),
-    );
-
-    let input = Crossword::new(String::from(
-        "
-  S *FRAN*BANAL
-  E *L  O*ALIBI
-BARITONES*N   O
-ENV* W *E D*  N
-**E  E*BROILERS
-RATEDR*AINTI***
-AMITY*B N *ACDC
-M M*AMALGAM*R  
-P E * L S*AMINO
-***ACIDY*GRATES
-ENDZONES*A  I**
-KIA*A A* R *C  
-EVILS*GOODTHING
-B  ET*L  E* S  
-YAYAS*ETON* M  
-",
-    ))
-    .unwrap();
-
-    let tmp_trie = trie.clone();
-
-    c.bench(
-        "simple_filler",
-        Benchmark::new("fill_20201012_crossword", move |b| {
-            let mut filler = SimpleFiller::new(tmp_trie.as_ref());
+            let input = std::fs::read_to_string("./grids/20201012_empty.txt")
+                .expect("failed to read input");
+            let input = Crossword::new(input).expect("failed to parse input");
 
             b.iter(|| {
                 assert!(filler.fill(black_box(&input)).is_ok());
