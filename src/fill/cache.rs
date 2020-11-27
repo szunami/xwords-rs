5792aa4ce2391c50c2f31343f7ab28f1bee6179f
Sam Szuflita <sam.szuflita@gmail.com>
2020-11-27 18:31:02 -0500

    Remove commented out code

--- src/fill/cache.rs
+++ src/fill/cache.rs
@@ -4,42 +4,6 @@ use rustc_hash::{FxHashMap, FxHasher};
 
 use crate::{crossword::CrosswordWordIterator, trie::Trie};
 
-// #[derive(Clone)]
-// pub struct CachedWordSet {
-//     words_cache: FxHashMap<u64, Vec<String>>,
-//     is_word_cache: FxHashMap<u64, bool>,
-// }
-
-// impl CachedWordSet {
-
-//     pub fn new() -> CachedWordSet {
-//         CachedWordSet{
-//             words_cache: FxHashMap::default(),
-//             is_word_cache: FxHashMap::default(),
-//         }
-//     }
-
-//     pub fn words(&mut self, iter: CrosswordWordIterator, trie: &Trie) -> &Vec<String> {
-//         let mut hasher = FxHasher::default();
-//         for c in iter.clone() {
-//             c.hash(&mut hasher);
-//         }
-//         let key = hasher.finish();
-
-//         self.words_cache.entry(key).or_insert_with(|| trie.words(iter))
-//     }
-
-//     pub fn is_word(&mut self, iter: CrosswordWordIterator, trie: &Trie) -> bool {
-//         let mut hasher = FxHasher::default();
-//         for c in iter.clone() {
-//             c.hash(&mut hasher);
-//         }
-//         let key = hasher.finish();
-
-//         *self.is_word_cache.entry(key).or_insert_with(|| trie.is_word(iter))
-//     }
-// }
-
 #[derive(Clone)]
 pub struct CachedWords {
     words_cache: FxHashMap<u64, Vec<String>>,
